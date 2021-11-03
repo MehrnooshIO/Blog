80b29927824cab5f457e7546737cd97450868e90
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-11-03 20:41:41 +0330

    added telemetry

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
 from routes.routes import blog_router
-
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from grpc import ssl_channel_credentials
 
 blog = FastAPI(
     debug=True,
@@ -10,6 +15,31 @@ blog = FastAPI(
 )
 my_blog = blog_router()
 blog.include_router(my_blog)
+
+
+# settings for observability
+blog_resource = Resource(attributes={
+    "service.name": "MyBlog"
+})
+
+blog_trace_provider = TracerProvider(resource=blog_resource)
+
+otlp_exporter = OTLPSpanExporter(
+    endpoint="api.honeycomb.io:443",
+    insecure=False,
+    credentials=ssl_channel_credentials(),
+    headers=(
+        ("x-honeycomb-team", "6e34d5a6fac444ef36f23d2e765b01a7"),
+    )
+)
+
+# register exporter with provider
+blog_trace_provider.add_span_processor(
+    BatchSpanProcessor(otlp_exporter)
+)
+
+# register trace provider
+trace.set_tracer_provider(blog_trace_provider)
 
 
 if __name__ == "__main__":
--- routes/routes.py
+++ routes/routes.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter
-
+from opentelemetry import trace
 
 def blog_router() -> APIRouter:
     router = APIRouter(prefix="/blog")
 
-    @router.get("/")
-    def all_posts():
-        """
-        :return: json format of all posts
-        """
-        return {"list": ["list of posts"]}
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("http-handler"):
+        with tracer.start_as_current_span("all_posts"):
+            @router.get("/")
+            def all_posts():
+                """
+                :return: json format of all posts
+                """
+                return {"list": ["list of posts"]}
 
     @router.post("/")
     def create_post():

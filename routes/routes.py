8443dcdb29de6ff33afee05dbf2d511e006cefc2
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2021-11-08 12:11:16 -0500

    Add basic structure of blog routes

--- routes/routes.py
+++ routes/routes.py
@@ -1,2 +1,33 @@
 from fastapi import APIRouter
 
+def create_blog_router():
+    router = APIRouter(
+        prefix="/blog",
+        tags=["blog"],
+    )
+
+    @router.get("/")
+    def get_blogs():
+        return {"message": "All posts"}
+
+    @router.get("/{blog_id}")
+    def get_blog(blog_id: int):
+        return {"message": f"Blog {blog_id}"}
+
+    @router.post("/")
+    def create_blog():
+        return {"message": "Create a new blog"}
+
+    @router.put("/{blog_id}")
+    def update_blog(blog_id: int):
+        return {"message": f"Update blog {blog_id}"}
+
+    @router.delete("/{blog_id}")
+    def delete_blog(blog_id: int):
+        return {"message": f"Delete blog {blog_id}"}
+
+    @router.get("/{tag}")
+    def get_blog_by_tag(tag: str):
+        return {"message": f"Get blogs by tag {tag}"}
+
+    return router
